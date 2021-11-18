f8b9a436b8b1356b09583191a4e28dc009f54325
joel-CM <cjoel1977@gmail.com>
2021-11-18 17:00:27 -0300

    falto una llave

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    application: {
-        cors: {
-            server: [
-                {
-                    origin: "https://pi-dog-front.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
-                    credentials: true
-                },
-                {
-                    origin: "http://pi-dog-front.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
-                    credentials: true
-                }
-            ]
-        }
-}
+  application: {
+    cors: {
+      server: [
+        {
+          origin: "https://pi-dog-front.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+          credentials: true,
+        },
+        {
+          origin: "http://pi-dog-front.herokuapp.com", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+          credentials: true,
+        },
+      ],
+    },
+  },
+};
